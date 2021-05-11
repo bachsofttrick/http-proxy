a6ed9842839f5984d483ef606e7aef48edf1fb20
bach_oneicc <bach.phan@oneicc.com>
2021-05-11 10:22:44 +0700

    add internal route

--- router.js
+++ router.js
@@ -14,6 +14,10 @@ for (const server of serverList) {
     // Print list of servers to check for accuracy
     console.log(`[${server.route}]`, server.host);
     router.use(server.route, proxy(server.host, options));
+    if (server.useInternal) {
+        console.log(`[${server.internalRoute}]`, server.host);
+        router.use(server.internalRoute, proxy(server.host, options));
+    }
 }
 
 module.exports = router;
--- serverList.js
+++ serverList.js
@@ -4,20 +4,27 @@ class Server {
     constructor(useLocal, route, host) {
         // Use localhost or external server
         this.useLocal = useLocal;
+        this.useInternal = false;
         this.route = route;
+        this.internalRoute = '/internal' + this.route;
         this.host = this.useLocal ? host : notUsingLocal
+    }
+
+    useInternalRoute() {
+        this.useInternal = true;
+        return this;
     }
 }
 
 const serverList = [
     new Server(false, '/activity', 'localhost:4005'),
-    new Server(false, '/config', 'localhost:4006'),
-    new Server(false, '/member', 'localhost:4002'),
+    new Server(true, '/config', 'localhost:4006'),
+    new Server(true, '/member', 'localhost:4002').useInternalRoute(),
+    new Server(true, '/mall', 'localhost:4007'),
     new Server(false, '/promotions', 'localhost:4008'),
     new Server(false, '/sale', 'localhost:4004'),
-    new Server(true, '/shop', 'localhost:4003'),
-    new Server(false, '/user', 'localhost:4001'),
-    new Server(true, '/van', 'localhost:4009')
+    new Server(false, '/shop', 'localhost:4003'),
+    new Server(true, '/user', 'localhost:4001').useInternalRoute(),
+    new Server(false, '/van', 'localhost:4009')
 ];
-
 module.exports = serverList;
