be837a3e9c74c037d29693fd1ae547889008a3b8
bach_oneicc <bach.phan@oneicc.com>
2021-06-07 10:11:43 +0700

    change member and sale address

--- serverList.js
+++ serverList.js
@@ -15,15 +15,15 @@ class Server {
 
 const serverList = [
     new Server(false, '/activity', 'localhost:4005'),
-    new Server(false, '/config', 'localhost:4006'),
+    new Server(true, '/config', 'localhost:4006'),
     new Server(true, '/integration', 'localhost:4011'),
-    new Server(true, '/mall', 'localhost:4010'),
-    new Server(true, '/member', 'localhost:4002'),
+    new Server(false, '/mall', 'localhost:4010'),
+    new Server(false, '/member', 'localhost:4004'),
     new Server(true, '/notification', 'localhost:4007'),
     new Server(false, '/promotions', 'localhost:4008'),
-    new Server(true, '/sale', 'localhost:4004'),
+    new Server(true, '/sale', 'localhost:4002'),
     new Server(true, '/shop', 'localhost:4003'),
-    new Server(true, '/user', 'localhost:4001'),
+    new Server(false, '/user', 'localhost:4001'),
     new Server(false, '/van', 'localhost:4009')
 ];
 
