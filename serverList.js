4e50c678d50167a36c6361314ab407574c1de45b
bach_oneicc <bach.phan@oneicc.com>
2021-07-15 14:01:18 +0700

    remake port to match sql port

--- serverList.js
+++ serverList.js
@@ -13,17 +13,18 @@ class Server {
     }
 }
 
+// Port are in accordance with SQL port
 const serverList = [
     new Server(false, '/activity', 'localhost:4005'),
     new Server(false, '/config', 'localhost:4006'),
-    new Server(false, '/ecommerce-shop', 'localhost:4012'),
-    new Server(true, '/integration', 'localhost:4011'),
-    new Server(true, '/mall', 'localhost:4010'),
-    new Server(false, '/member', 'localhost:4004'),
+    new Server(false, '/ecommerce-shop', 'localhost:4010'),
+    new Server(true, '/integration', 'localhost:4013'),
+    new Server(true, '/mall', 'localhost:4012'),
+    new Server(false, '/member', 'localhost:4002'),
     new Server(true, '/notification', 'localhost:4007'),
     new Server(false, '/promotions', 'localhost:4008'),
-    new Server(false, '/report', 'localhost:4013'),
-    new Server(false, '/sale', 'localhost:4002'),
+    new Server(false, '/report', 'localhost:4011'),
+    new Server(false, '/sale', 'localhost:4004'),
     new Server(false, '/shop', 'localhost:4003'),
     new Server(false, '/user', 'localhost:4001'),
     new Server(false, '/van', 'localhost:4009')
