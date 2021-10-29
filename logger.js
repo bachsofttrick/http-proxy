ae7711e3e97a07d6d1cc6238c4cb96a1eda16fbc
bach_oneicc <bach.phan@oneicc.com>
2021-10-29 14:24:34 +0700

    update package

--- logger.js
+++ logger.js
@@ -1,8 +1,8 @@
 const logger = (req, res, next) => {
     const today = new Date();
     console.log(today.toLocaleString(), req.method, req.originalUrl);
-    if (Object.keys(req.query).length > 0) console.log('query', req.query);
-    if (Object.keys(req.body).length > 0) console.log('body', req.body);
+    if (Object.keys(req.query).length > 0) console.log('query', JSON.stringify(req.query));
+    if (Object.keys(req.body).length > 0) console.log('body', JSON.stringify(req.body));
     next();
 };
 
