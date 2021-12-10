f041258a9c4f922caf067c1ed6dbdc890076ef6b
bach_oneicc <bach.phan@oneicc.com>
2021-12-10 09:53:45 +0700

    turn off parseReqBody for upload large file

--- router.js
+++ router.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 const options = {
     limit: null,
+    parseReqBody: false,
     proxyReqPathResolver: (req) => {
         return req.originalUrl;
     }
--- serverList.js
+++ serverList.js
@@ -3,13 +3,13 @@ const { Microservice, BlockchainService, SafeIdService } = require('./server-cla
 // Port are in accordance with SQL port (microservice)
 const serverList = [
     new Microservice(false, '/activity', 'localhost:4005'),
-    new Microservice(true, '/balance', 'localhost:4016'),
+    new Microservice(false, '/balance', 'localhost:4016'),
     new Microservice(false, '/config', 'localhost:4006'),
-    new Microservice(true, '/ecommerce-shop', 'localhost:4010'),
+    new Microservice(false, '/ecommerce-shop', 'localhost:4010'),
     new Microservice(true, '/integration', 'localhost:4013'),
     new Microservice(true, '/mall', 'localhost:4012'),
     new Microservice(false, '/member', 'localhost:4002'),
-    new Microservice(true, '/notification', 'localhost:4007'),
+    new Microservice(false, '/notification', 'localhost:4007'),
     new Microservice(false, '/promotions', 'localhost:4008'),
     new Microservice(false, '/report', 'localhost:4011'),
     new Microservice(false, '/sale', 'localhost:4004'),
